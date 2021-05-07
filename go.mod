0f9c9632286e35a0cb48b1a40e1edd86489c57c0
irevenko <kekalkaru20@gmail.com>
2021-05-07 23:08:28 +0300

    Update main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/irevenko/weebsay
 
 go 1.16
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20200609044655-c4b36f998cf2 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,74 +4,62 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
 
-const importantFace = `
-             ╓∩H▒▒▒▒▒▒▒▒▒∩╖.            
-         ╓H▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╖         
-       ╖▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╥.      
-     ╖▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╗     
-    ▒▄▄▄▄▄▄▄▄▄▄▒▒▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▄▄▒    
-   ▀███▓▓▓▓▓▓▓▓████▓▓████▓▓▓▓▓▓▓▓███▀   
-  ╓▒▒██████████████▒▒▀█████████████▌▒╥  
-  ║▒▒█████████████▒▒▒▒█████████████▒▒▒  
-  ▒▒▒▒███████████▀▒▒▒▒▒███████████▒▒▒▒  
-  ╙▒▒▒▒▒▀▀▀██▀▀▒▒▒▒▒▒▒▒▒▒▀▀███▀▀▒▒▒▒▒║  
-   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒▒▒▒▒▒▒▒   
-    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▄█▀░▒▒▒▒▒▒▒   
-     ║▒▒▒▒▒▒▒▒▒█████████▀▀▒▒▒▒▒▒▒▒║    
-      ╙║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╜      
-         ╝▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╝        
-            ╙╝╣▒▒▒▒▒▒▒▒▒▒╣╝╜           
+const cherryBlossom = `
+               ,╥╦╦  ╓╦╥,                 
+             ╓▓▒▒▒▒▒▒▒▒▒▒▓╗               
+             ▒▒▒▒▒░░░░▒▒▒▒▒N              
+            j▒▒▒▒░░░░░░░▒▒▒[              
+   ,╦╣╣╢╣╗╖  ╣▒▒▒░░░░░░░▒▒▒  ╓╗╣╢╢╬╦,     
+ ╒▓▒▒▒▒▒▒▒▒▒╢╖▒▒▒▒▒▒▒▒▓█▒▒╗╣▒▒▒▒▒▒▒▒▒▓╕   
+  ╣▒▒▒░░░░▒▒▒▒▒▒╢╢█▀╣╣ ╣▒▒▒▒▒▒░░░░░▒▒▒    
+g╣▒▒░░░░░░░▒▀▀╙╨▓╢╢╢▒▒╫▓╝╜▀▀▒░░░░░░░▒▒╢@  
+▓▒▒▒▒░░░░░░▒▒▒▓@╖▒▒▒▒▒▒╫@╣╢▒▒▄░░░░░▒▒▒▒╫  
+ ▓▒▒▒▒▒▒▒▒▒▒▒╢▀▓▓▒▒▒▒▒▒▓φ@@@╫▀░▒▒▒▒▒▒▒╢   
+  "╩╣▒▒▒▒▒▒▒╣╢╢╜░╠▓▓▓▓▓▒▓▓▒▒▒▒▒▒▒▒▒╣╩"    
+        ╓╣▒▒▓▄╓╣▓▄╫╫╢░╟╢▒▀▒▒▒▒╢╦          
+     ,@▒▒▒▒▒░░▒▒▒▒╢╫╣╣▄▓▒▒▒▒▒▒▒▒▒╢,       
+     ▒▒▒▒▒░░░░░▒▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒L      
+    ]╣▒▒▒░░░░░░░▒▒▒╫║▒▒▒░░░░░░░▒▒▒╢╛      
+     ▓▒▒▒▒▒░░░░▒▒▒▒  ╣▒▒▒▒░░░░▒▒▒▒▓       
+      "╙ ║▒▒▒▒▒▒▒╫    ╣▒▒▒▒▒▒▒▒           
+          ╩╩╬╣Ñ╩        ╨╩╬╬▓╩            
 `
 
-const importantFinger = `
-                         ╓╗╖╖           
-                        ╒▒▒▒▒╣          
-                        ╞▒▒▒▒╢          
-                        ╟▒▒▒▒╢          
-                        ╟▒▒▒▒╢          
-                        ╟╣▒▒▒╢          
-              ╦╣╣╬╦▓▒▒▒▒▓╣▒▒▒╢          
-        ╫▒▒▒▒▓╣▒▒▒╫▌▒▒▒▒╫╣▒▒▒╢          
-        ╣▒▒▒▒▓▒▒▒▒╢▓▒▒▒▒╫╣▒▒▒╫          
-        ▓╣╣╣▓▓▓▓╣╢╢▓╣╢▒▒╢╢▒▒▒╢═         
-        ▓▓▓▓▓▓▓▓╣╣╣╣▒╢╢▒▒▒▒▒▒╢╣         
-        ▓╣╣╣╣╢╢╢▓▓▓▓▓▓▓╣╣▒▒▒▒▒▒▓        
-        ╟╣▒▒▒▒▒╢╢▓▓▓▓▓▓▓▓▒▒▒▒▒╢▓        
-         ╣▒▒▒▒▒▒▒▒╢╢╢╢╣╣╣▒▒▒▒╢╫▓        
-         ▐╣▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢▓▓         
-          ▐▓╣▒▒▒▒▒▒▒▒▒▒▒╣╢╢▓▓▓          
-            ▓▓▓╣╣▒▒▒▒╢╢╢▓▓▓▓            
-               ╙▀▀▓▓▓▓▓▀▀╙ 
-`
+const apiLink = "https://animechan.vercel.app/api/random"
 
 func main() {
-	resp, err := http.Get("https://animechan.vercel.app/api/random")
+	resp, err := http.Get(apiLink)
 	if err != nil {
-		print(err)
+		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		print(err)
+		log.Fatal(err)
 	}
 
-	var animeResp Response
-	json.Unmarshal(body, &animeResp)
+	var quoteResp AnimeChanResponse
+	json.Unmarshal(body, &quoteResp)
 
-	imgSlice := strings.Split(importantFinger, "\n")
-	// quoteSlice := strings.Split(animeResp.Quote, " ")
+	imgSlice := strings.Split(cherryBlossom, "\n")
+	quotesSlice := strings.Split(quoteResp.Quote, " ")
+	quote := chunkSlice(quotesSlice, 9)
 
+	quoteCounter := 0
 	for i, v := range imgSlice {
 		if i > 0 && i < len(imgSlice)-1 {
-			if i == 4 {
-				fmt.Println(v + " " + animeResp.Quote)
-			} else if i == 6 {
-				fmt.Println(v + "                     " + animeResp.Char)
+			if i > 3 && quoteCounter < len(quote) {
+				fmt.Println(v + strings.Join(quote[quoteCounter], " "))
+				quoteCounter++
+				if quoteCounter == len(quote) {
+					fmt.Println(v + "  - " + quoteResp.Char + " from " + quoteResp.Anime)
+				}
 			} else {
 				fmt.Println(v)
 			}
@@ -79,7 +67,26 @@ func main() {
 	}
 }
 
-type Response struct {
+func chunkSlice(slice []string, chunkSize int) [][]string {
+	var chunks [][]string
+
+	for {
+		if len(slice) == 0 {
+			break
+		}
+
+		if len(slice) < chunkSize {
+			chunkSize = len(slice)
+		}
+
+		chunks = append(chunks, slice[0:chunkSize])
+		slice = slice[chunkSize:]
+	}
+
+	return chunks
+}
+
+type AnimeChanResponse struct {
 	Anime string `json:"anime"`
 	Char  string `json:"character"`
 	Quote string `json:"quote"`
